8678be508c7d9b3e064ae811450404c8926ed918
Mateusz Król <m_krol@poczta.wwsi.edu.pl>
2017-02-01 21:10:37 +0100

    Fix condition

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 function getTriangleArea(a, h) {
     if (a <= 0 || h <= 0) {
-        console.log('Nie prawidlowe dane');
-
+        return getTriangleArea.toString = 'Nie prawidłowe dane';
     }
-
     return a * h / 2;
-
 }
 
 var triangleArea1 = getTriangleArea(4, 6);
 var triangleArea2 = getTriangleArea(12, 4);
 var triangleArea3 = getTriangleArea(4, 33);
+var triangleArea4 = getTriangleArea(-4, 3);
+var triangleArea5 = getTriangleArea(2, -6);
 
-console.log('Pole pierwszego trojkata wynosi: ' + triangleArea1 + '.  Pole drugiego trojkota wynosi: ' + triangleArea2 + '.  Pole trzeciego trojkata wynosi: ' + triangleArea3);
+console.log('Pole pierwszego trojkata wynosi: ' + triangleArea1);
+console.log('Pole drugiego trojkata wynosi: ' + triangleArea2);
+console.log('Pole trzeciego trojkata wynosi: ' + triangleArea3);
+console.log('Pole czwartego trojkata wynosi: ' + triangleArea4);
+console.log('Pole piątego trojkata wynosi: ' + triangleArea5);
