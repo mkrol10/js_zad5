f23f96d06d9a4b0cb6deb8a19a0695d580fea15a
Mateusz Król <m_krol@poczta.wwsi.edu.pl>
2017-02-01 21:17:36 +0100

    fix condition

--- js/script.js
+++ js/script.js
@@ -10,9 +10,11 @@ var triangleArea2 = getTriangleArea(12, 4);
 var triangleArea3 = getTriangleArea(4, 33);
 var triangleArea4 = getTriangleArea(-4, 3);
 var triangleArea5 = getTriangleArea(2, -6);
+var triangleArea6 = getTriangleArea(1, 6);
 
 console.log('Pole pierwszego trojkata wynosi: ' + triangleArea1);
 console.log('Pole drugiego trojkata wynosi: ' + triangleArea2);
 console.log('Pole trzeciego trojkata wynosi: ' + triangleArea3);
 console.log('Pole czwartego trojkata wynosi: ' + triangleArea4);
 console.log('Pole piątego trojkata wynosi: ' + triangleArea5);
+console.log('Pole piątego trojkata wynosi: ' + triangleArea6);
